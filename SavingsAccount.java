65dde21bf8e576b8bcc6d1669b8da80f756c6ecd
Subhajit Sahu <wolfram77@gmail.com>
2020-11-13 15:29:38 +0530

    :bus: 2 condition savings account

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -9,21 +9,71 @@ import java.util.concurrent.locks.*;
 // transfer() from an account also blocks until the
 // source account has the necessary balance.
 // Preferred withdrawls are performed, if waiting.
+// 
+// Note: Using 2 separate conditions avoids unnecessary
+// waking up of non-preferred withdrawls when preferred
+// withdrals are present.
 
 class SavingsAccount {
   Lock lock;
-  Condition hasBalance;
+  Condition hasNormal;
+  Condition hasPreferred;
   int balance;
+  int preferred;
   // lock: common lock
-  // hasBalance: signalled when account has balance
+  // hasNormal: account has balance (no preferred deposits)
+  // hasPreferred: account has balance (some preferred)
   // balance: amount of currency in account
+  // preferred: pending preferred withdrawls
+
 
   public SavingsAccount() {
     lock = new ReentrantLock();
+    hasNormal = lock.newCondition();
+    hasPreferred = lock.newCondition();
+    balance = 0;
   }
 
+
   // 1. Acquire common lock.
-  // 2. Wait until there is no other gender.
-  // 3. Increment my gender count.
+  // 2. Deposit to account.
+  // 3. Signal that account has currency.
   // 4. Release common lock.
+  public void deposit(int k) {
+    lock.lock();
+    balance += k;
+    signal();
+    lock.unlock();
+  }
+
+
+  // 1. Acquire common lock.
+  // 2. Update pending preferred withdrawls.
+  // 3. Wait until sufficient balance available.
+  // 4. Withdraw from account.
+  // 5. Update pending preferred withdrawls.
+  // 6. Release common lock.
+  public void withdraw(int k, boolean pref)
+  throws InterruptedException {
+    lock.lock();           // 1
+    if (pref) preferred++; // 2
+
+    while (balance<k) await(pref); // 3
+    balance -= k;                  // 4
+    
+    if (pref) preferred--; // 5
+    lock.unlock();         // 6
+  }
+
+
+  private void signal() {
+    if (preferred>0) hasPreferred.signal();
+    else hasNormal.signal();
+  }
+
+  private void await(boolean pref)
+  throws InterruptedException {
+    if (pref) hasPreferred.await();
+    else hasNormal.await();
+  }
 }
